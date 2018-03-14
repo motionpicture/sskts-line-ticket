6560e309cdfbba7b192fa6c0473a4d945b4b693a
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-03-14 01:46:27 +0000

    # SSKTS-868 ### Changed - update pecorino-api-nodejs-client.

--- src/app/middlewares/authentication.ts
+++ src/app/middlewares/authentication.ts
@@ -72,8 +72,8 @@ export default async (req: Request, res: Response, next: NextFunction) => {
 
 export async function sendLoginButton(user: User) {
     // tslint:disable-next-line:no-multiline-string
-    let text = `ログインしてください。一度ログイン後、顔写真を登録すると、次回からFace Loginを使用できます。
-また、会員未登録の方は、「Sign In」のリンク先に「Sign Up」のリンクがございますので、そちらより会員登録をお願いいたします。`;
+    let text = `ログインしてください。一度ログイン後、顔写真を登録すると次回からFace Loginを使用できます。
+また、会員登録は「Sign In」のリンク先の「Sign Up」リンクより行えます。`;
     const actions: any[] = [
         {
             type: 'uri',
